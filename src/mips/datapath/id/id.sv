5b444a1e035a98c973c796e53438fe7a1d924352
endereye <endereyewxy@gmail.com>
2020-12-29 23:03:20 +0800

    用接口重写gpr

--- src/mips/datapath/id/id.sv
+++ src/mips/datapath/id/id.sv
@@ -25,39 +25,39 @@ module id(
     output logic         er             );
 
     decode decode_(
-        .inst(inst),
-        .ityp(ityp),
-        .oper(oper),
-        .func(func),
-        .imme(imme),
+        .inst   (inst   ),
+        .ityp   (ityp   ),
+        .oper   (oper   ),
+        .func   (func   ),
+        .imme   (imme   ),
         .rs_regf(rs_regf),
         .rt_regf(rt_regf),
         .rd_regf(rd_regf),
-        .sy(sy),
-        .bp(bp),
-        .ri(ri),
-        .er(er));
+        .sy     (sy     ),
+        .bp     (bp     ),
+        .ri     (ri     ),
+        .er     (er     ));
 
     logic `W_DATA srca;
     logic `W_DATA srcb;
 
     brcsrc brcsrc_(
-        .forward_rs(forward_rs),
-        .forward_rt(forward_rt),
+        .forward_rs     (forward_rs     ),
+        .forward_rt     (forward_rt     ),
         .forward_rs_data(forward_rs_data),
         .forward_rt_data(forward_rt_data),
-        .rs_data(rs_data),
-        .rt_data(rt_data),
-        .source_a(srca),
-        .source_b(srcb));
+        .rs_data        (rs_data        ),
+        .rt_data        (rt_data        ),
+        .source_a       (srca           ),
+        .source_b       (srcb           ));
     
     branch branch_(
-        .oper(oper),
-        .imme(imme),
-        .source_a(srca),
-        .source_b(srcb),
-        .pc(pc),
-        .branch(branch),
+        .oper       (oper       ),
+        .imme       (imme       ),
+        .source_a   (srca       ),
+        .source_b   (srcb       ),
+        .pc         (pc         ),
+        .branch     (branch     ),
         .branch_addr(branch_addr));
     
 endmodule
--- src/mips/datapath/mm/except.sv
+++ src/mips/datapath/mm/except.sv
@@ -2,29 +2,24 @@
 `include "defines.vh"
 
 module except(
-    input  logic         slot       ,
-    input  logic `W_INTV intr_vect  ,
-           ader.master   ibus       ,
-           ader.master   dbus       ,
-    input  logic         sy         ,
-    input  logic         bp         ,
-    input  logic         ri         ,
-    input  logic         ov         ,
-    input  logic         er         ,
-    input  logic `W_ADDR er_epc     ,
-    input  logic `W_ADDR pc         ,
-    output logic         except     ,
-    output logic `W_ADDR except_addr,
-    output logic         cp0_en     ,
-    output logic         cp0_bd     ,
-    output logic         cp0_exl    ,
-    output logic `W_EXCC cp0_exc    ,
-    output logic `W_ADDR cp0_epc    ,
-    output logic `W_ADDR cp0_bva    );
+    input         logic         slot       ,
+    input         logic `W_INTV intr_vect  ,
+    ader.master                 ibus       ,
+    ader.master                 dbus       ,
+    input         logic         sy         ,
+    input         logic         bp         ,
+    input         logic         ri         ,
+    input         logic         ov         ,
+    input         logic         er         ,
+    input         logic `W_ADDR er_epc     ,
+    input         logic `W_ADDR pc         ,
+    output        logic         except     ,
+    output        logic `W_ADDR except_addr,
+    regf_e.master               cp0        );
     
     logic actual_except;
     
-    assign {actual_except, cp0_exc} =
+    assign {actual_except, cp0.exc} =
         (intr_vect != 0)        ? {1'b1, `EXCC_INT } :
          ibus.adel              ? {1'b1, `EXCC_ADEL} :
          ri                     ? {1'b1, `EXCC_RI  } :
@@ -36,11 +31,11 @@ module except(
     assign except      = actual_except | er;
     assign except_addr = actual_except ? 32'hbfc00380 : er ? er_epc : 0;
     
-    assign cp0_en  = actual_except;
-    assign cp0_bd  = actual_except & slot;
-    assign cp0_exl = actual_except;
-    assign cp0_epc = slot ? (pc - 4) : pc;
-    assign cp0_bva = ibus.adel ? ibus.addr : (dbus.adel | dbus.ades) ? dbus.addr : 0;
+    assign cp0.we  = actual_except;
+    assign cp0.bd  = actual_except & slot;
+    assign cp0.exl = actual_except;
+    assign cp0.epc = slot ? (pc - 4) : pc;
+    assign cp0.bva = ibus.adel ? ibus.addr : (dbus.adel | dbus.ades) ? dbus.addr : 0;
     
 endmodule
 
