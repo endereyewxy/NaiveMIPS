081f26a5683e128caecd46fd153549a1aa7edf9a
endereye <endereyewxy@gmail.com>
2020-12-29 22:50:42 +0800

    用接口重写某些模块

--- src/mips/datapath/wb/wb.sv
+++ src/mips/datapath/wb/wb.sv
@@ -2,13 +2,12 @@
 `include "defines.vh"
 
 module wb(
-    input  logic `W_OPER oper        ,
-    input  logic `W_REGF rd_regf     ,
-    input  logic `W_DATA rd_data_a   ,
-    input  logic `W_DATA rd_data_b   ,
-    output logic `W_DATA rd_data     ,
-    output logic `W_REGF rd_regf_out);
-
-    regsrc regsrc_(oper, rd_regf, rd_data_a, rd_data_b, rd_data, rd_regf_out);
-
+    input         logic `W_OPER oper        ,
+    input         logic `W_REGF rd_regf     ,
+    input         logic `W_DATA rd_data_a   ,
+    input         logic `W_DATA rd_data_b   ,
+    regf_w.master               rd          );
+    
+    regsrc regsrc_(.*);
+    
 endmodule
