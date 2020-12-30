167ba37ca54532f1643dca8d9fd0cd7200c570ef
endereye <endereyewxy@gmail.com>
2020-12-30 13:25:50 +0800

    新增一条信号，统一格式

--- src/mips/datapath/wb/wb.sv
+++ src/mips/datapath/wb/wb.sv
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 `include "defines.vh"
 
 module wb(
@@ -6,9 +6,11 @@ module wb(
     input  logic `W_REGF rd_regf     ,
     input  logic `W_DATA rd_data_a   ,
     input  logic `W_DATA rd_data_b   ,
+    input  logic `W_ADDR pc          ,
     output logic `W_DATA rd_data     ,
     output logic `W_REGF rd_regf_out);
 
-    regsrc regsrc_(oper, rd_regf, rd_data_a, rd_data_b, rd_data, rd_regf_out);
+    regsrc regsrc_(oper, rd_regf, rd_data_a, rd_data_b, pc, rd_data, rd_regf_out);
 
 endmodule
+
