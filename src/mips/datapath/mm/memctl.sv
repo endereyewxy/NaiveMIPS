c90024694332591acce3a170f40cc5ba2fda772e
endereye <endereyewxy@gmail.com>
2020-12-30 13:23:26 +0800

    删除无用线路，统一格式

--- src/mips/datapath/mm/memctl.sv
+++ src/mips/datapath/mm/memctl.sv
@@ -12,14 +12,14 @@ module memctl(
     output logic `W_DATA dbus_data
     );
 
-    assign dbus_en   = `IS_OPER_MM(oper)    ? 1'b1       : 1'b0   ;
+    assign dbus_en = `IS_OPER_MM(oper);
 
-    assign dbus_we   =  oper == `OPER_SB    ? 4'b0001    :
-                        oper == `OPER_SH    ? 4'b0011    :
-                        oper == `OPER_SW    ? 4'b1111    : 4'b0000;
+    assign dbus_we = oper == `OPER_SB ? 4'b0001 :
+                     oper == `OPER_SH ? 4'b0011 :
+                     oper == `OPER_SW ? 4'b1111 : 4'b0000;
+    
     assign dbus_addr =  source_addr;
 
-    assign dbus_data =  `IS_OPER_JB(oper)                                        ? pc + 8      :
-                        (oper == `OPER_SB | oper == `OPER_SH | oper == `OPER_SW) ? source_data : source_addr;
+    assign dbus_data = (oper == `OPER_SB | oper == `OPER_SH | oper == `OPER_SW) ? source_data : source_addr;
 endmodule
 
